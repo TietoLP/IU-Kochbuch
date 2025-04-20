86d5349035b1596fc8d9a274082350ccb0a0a492
Tieto Draco <leonkinski@gmail.com>
2025-04-20 12:49:10 +0200

    Initial commit: IU-Kochbuch

---
+++ src/main/java/com/meinname/kochbuch/kochbuch/IuKochbuchApplication.java
@@ -0,0 +1,13 @@
+package com.meinname.kochbuch.kochbuch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IuKochbuchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IuKochbuchApplication.class, args);
+	}
+
+}

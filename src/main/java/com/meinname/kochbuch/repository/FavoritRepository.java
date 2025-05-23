9a4dfa93e246a0e8800286eccb727561f648b09a
quempir <quimpery0@gmail.com>
2025-05-23 21:56:21 +0200

    Bilder

--- src/main/java/com/meinname/kochbuch/repository/FavoritRepository.java
+++ src/main/java/com/meinname/kochbuch/repository/FavoritRepository.java
@@ -1,6 +1,8 @@
 package com.meinname.kochbuch.repository;
 
 import com.meinname.kochbuch.model.Favorit;
+import com.meinname.kochbuch.model.FavoritId;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FavoritRepository extends JpaRepository<Favorit, Long> {}
+public interface FavoritRepository extends JpaRepository<Favorit, FavoritId> {}
